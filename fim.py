5401f2175faf51215671c02f5f096b384a05ba66
Abdeslam Moussaoui <moussaouii.abdel@gmail.com>
2022-10-20 22:54:15 +0100

    compare 2 files

--- fim.py
+++ fim.py
@@ -1,4 +1,6 @@
 import hashlib
+
+hash_value='eba78765396a7b1aa00a24fd92e1a2a64b1012e5'
 def sha1(filename):
     BUF_SIZE = 65536  # read stuff in 64kb chunks!
     sha1 = hashlib.sha1()
@@ -12,6 +14,18 @@ def sha1(filename):
     
     
 
-file = 'H:/github/File_integrity_Monitoring/text.txt'
-file_hash = sha1(file)
-print ("the hash of the file is \n",file_hash)
+original_file = 'H:/github/File_integrity_Monitoring/original_file.txt'
+original_file_hash = sha1(original_file)
+
+file_copy = 'H:/github/File_integrity_Monitoring/file_copy.txt'
+file_copy_hash = sha1(file_copy)
+
+
+#print ("the hash of the file is \n",file_hash)
+
+if original_file_hash == file_copy_hash:
+	print("the origanal and the copy file are the same")
+
+else:
+	print("the content in the copy file changed")
+	
