49255d577218a697ed2acae8a55981627602ce4b
Abdeslam Moussaoui <moussaouii.abdel@gmail.com>
2022-10-20 03:56:45 +0100

    Working

---
+++ fim.py
@@ -0,0 +1,17 @@
+import hashlib
+def sha1(filename):
+    BUF_SIZE = 65536  # read stuff in 64kb chunks!
+    sha1 = hashlib.sha1()
+    with open(filename, 'rb') as f:
+        while True:
+            data = f.read(BUF_SIZE)
+            if not data:
+                break
+            sha1.update(data)
+    return sha1.hexdigest()
+    
+    
+
+file = 'H:/github/File_integrity_Monitoring/text.txt'
+file_hash = sha1(file)
+print ("the hash of the file is \n",file_hash)
